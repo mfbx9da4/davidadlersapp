cb190206536fb141c8039b6838ca378a31626312
David Adler <dalberto.adler@gmail.com>
2013-05-07 15:08:53 -0400

    t2

--- youtube_auth.py
+++ youtube_auth.py
@@ -23,13 +23,13 @@ class YtAuthPage(BaseHandler):
             redirect( flow.step1_get_authorize_url() )
             # credentials = flow.step2_exchange(code)
             #auth_url = 'https://accounts.google.com/o/oauth2/auth?'
-            client_id = '788984753858.apps.googleusercontent.com'
+            client_id1 = '788984753858.apps.googleusercontent.com'
             redirect_uri = 'http://davidadlersapp.appspot.com/yt_handler'
             scope1 = 'http://www.googleapis.com/auth/youtube'
             #response_type = '&response_type=code'
             #access_type = '&access_type=offline'
          	#return self.redirect(auth_url + client_id + redirect_uri + scope + response_type + access_type)
-            flow = OAuth2WebServerFlow(client_id = client_id,
+            flow = OAuth2WebServerFlow(client_id = client_id1,
                            client_secret='your_client_secret',
                            scope=scope1,
                            redirect_uri=redirect_uri)
