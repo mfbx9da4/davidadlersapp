adf83ab425120e3666d8a661233ae97c2c163db4
David Adler <dalberto.adler@gmail.com>
2013-05-08 13:33:13 -0400

    return to old version

--- youtube_auth.py
+++ youtube_auth.py
@@ -1,5 +1,6 @@
 from udacity.blog.object_models import BaseHandler
 from session import SessionHandler
+from webob import Request
 
 
 # '/yt_handler'
@@ -9,6 +10,16 @@ class YtPage(BaseHandler, SessionHandler):
             self.session['error'] = 'access_denied'
             return self.redirect( self.session.get('link'))
         else:
+            http = Reuest()
+            http.method = 'POST'
+            
+            code = 'code='+self.request.get('code')
+            client_id = '&client_id=788984753858.apps.googleusercontent.com'
+            redirect_uri = '&redirect_uri=
+            client_secret = '&client_secret=my-super-secret-key'
+            grant_type = '&grant_type=autorization_code'
+            
+            http.body = 
             pass
 
     def post(self):
@@ -19,12 +30,6 @@ class YtAuthPage(BaseHandler):
         def get(self):
             #redirect( flow.step1_get_authorize_url() )
             #credentials = flow.step2_exchange(code)
-            auth_url = 'https://accounts.google.com/o/oauth2/auth?'
-            client_id = '&client_id=788984753858.apps.googleusercontent.com'
-            redirect_uri = '&redirect_uri=https://davidadlersapp.appspot.com/yt_handler'
-            scope = '&scope=https://www.googleapis.com/auth/youtube'
-            response_type = '&response_type=code'
-            access_type = '&access_type=offline'
             return self.redirect(auth_url + client_id + redirect_uri + scope + response_type + access_type)
             #flow = OAuth2WebServerFlow(client_id = client_id1, client_secret='your_client_secret',scope=scope1,redirect_uri=redirect_uri)
             #redirect( flow.step1_get_authorize_url() )
