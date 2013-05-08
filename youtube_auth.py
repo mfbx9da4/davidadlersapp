b1cc7ce36cc733e5319c30c0266f5d96d63e6670
David Adler <dalberto.adler@gmail.com>
2013-05-08 12:59:58 -0400

    return to old version

--- youtube_auth.py
+++ youtube_auth.py
@@ -21,7 +21,7 @@ class YtAuthPage(BaseHandler):
             auth_url = 'https://accounts.google.com/o/oauth2/auth?'
             client_id = '&client_id=788984753858.apps.googleusercontent.com'
             redirect_uri = '&redirect_uri=http://davidadlersapp.appspot.com/yt_handler'
-            scope = '&scope=http://www.googleapis.com/auth/youtube'
+            scope = '&scope=https://www.googleapis.com/auth/youtube'
             response_type = '&response_type=code'
             access_type = '&access_type=offline'
             return self.redirect(auth_url + client_id + redirect_uri + scope + response_type + access_type)
