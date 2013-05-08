351f7b2e1dcfa3573b11c10e1503c02b33bcf290
David Adler <dalberto.adler@gmail.com>
2013-05-08 12:56:17 -0400

    return to old version

--- fb_yt.py
+++ fb_yt.py
@@ -11,7 +11,7 @@ class fb(BaseHandler, SessionHandler):
     def get(self):
         # test if the token 'code' exists, if it doesn't then it ask youtube a
         # a auth token
-        if not self.session.get('code'):
+        if self.session.get('code') == None:
             self.session['link'] = 'http://davidadlersapp.appspot.com/fb'
             self.redirect('yt_auth')
         else:
